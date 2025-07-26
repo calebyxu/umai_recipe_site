3132ec7f3be2d6a4ee87e072b82c43130a7ecc9e
calebyxu <calebxu1234@gmail.com>
2025-07-25 17:17:23 -0700

    testing routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,15 @@ export default function Home() {
     async function getData() {
         const sql = neon(process.env.DATABASE_URL!);
         const data = await sql`SELECT * FROM posts;`;
-        console.log(data);
-        return data;
+        return "hello";
     }
 
+    const tableData = getData();
+
     return (
-        <h1>hello this is the home page</h1>
+        <>
+            <h1>hello this is the home page</h1>
+            <h2>{tableData}</h2>
+        </>
     );
 }
