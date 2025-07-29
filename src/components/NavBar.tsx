9845a377c72b806457a103a969feebcbda844fee
calebyxu <calebxu1234@gmail.com>
2025-07-29 14:36:52 -0700

    added navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,20 @@
-
+import Link from 'next/link';
+import '../components/component.css';
+import umaiBlack from '../../public/img/UMAI-ICON-FINAL-02.svg';
 
 export default function NavBar() {
     return (
-        <>
-            <h1 style={{ color: 'white'}}>This is the navbar</h1>
-        </>
-    );
+        <header className="topnavBackground" id="navbar">
+            <div className='topnav'>
+                <div id='googleLogin'>
+                    <Link href="/"><img src={umaiBlack}></img></Link>
+                </div>
+                <div className='topnavLinks'>
+                    <Link href='/wip'>Add Recipe</Link>
+                    <Link href='/discover'>Discover</Link>
+                    <Link href='/wip'>Your Recipes</Link>
+                </div>
+            </div>
+        </header> 
+);
 }
