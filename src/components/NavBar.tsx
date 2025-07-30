f0d1a35b48cde2050d0698d613a82a5046ce214c
calebyxu <calebxu1234@gmail.com>
2025-07-29 18:27:38 -0700

    adding google login

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,6 +8,22 @@ export default function NavBar() {
             <div className='topnav'>
                 <div id='googleLogin'>
                     <Link href="/"><Image src='./img/umaiBlackLogo.svg' className='topnavImg' alt='image'></Image></Link>
+                    <div id="g_id_onload"
+                        data-client_id="1063240373526-setg3qv32eos774o69n7erif729utnos.apps.googleusercontent.com"
+                        data-context="signin"
+                        data-ux_mode="popup"
+                        data-callback="handleCredentialResponse"
+                        data-auto_prompt="false">
+                    </div>
+
+                    <div className="g_id_signin"
+                        data-type="standard"
+                        data-shape="rectangular"
+                        data-theme="outline"
+                        data-text="signin_with"
+                        data-size="large"
+                        data-logo_alignment="left">
+                    </div>
                 </div>
                 <div className='topnavLinks'>
                     <Link href='/wip'>Add Recipe</Link>
@@ -15,6 +31,6 @@ export default function NavBar() {
                     <Link href='/wip'>Your Recipes</Link>
                 </div>
             </div>
-        </header> 
-);
+        </header>
+    );
 }
