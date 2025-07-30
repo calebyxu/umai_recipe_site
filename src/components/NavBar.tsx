7d8ad71bab87e2eea0b102fcc8ba2d48eaee84d5
calebyxu <calebxu1234@gmail.com>
2025-07-30 16:13:30 -0700

    bug fixing

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,6 +5,61 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function NavBar() {
 
+    function decodeJWT(token: string) {
+        let base64Url = token.split(".")[1];
+        let base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+        let jsonPayload = decodeURIComponent(
+            atob(base64)
+                .split("")
+                .map(function (c) {
+                    return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+                })
+                .join("")
+        );
+        return JSON.parse(jsonPayload);
+    }
+    async function handleCredentialResponse(response: {credential: string}) {
+
+        console.log("Encoded JWT ID token: " + response.credential);
+
+        const responsePayload = decodeJWT(response.credential);
+
+        console.log("Decoded JWT ID token fields:");
+        console.log("  Full Name: " + responsePayload.name);
+        console.log("  Given Name: " + responsePayload.given_name);
+        console.log("  Family Name: " + responsePayload.family_name);
+        console.log("  Unique ID: " + responsePayload.sub);
+        console.log("  Profile image URL: " + responsePayload.picture);
+        console.log("  Email: " + responsePayload.email);
+
+        /*fetch api doesn't work with current app since it doesn't reload page*/
+        // try {
+        //     const response = await fetch("https://umai-site-fc33abfe2d2d.herokuapp.com/home", {
+        //         method: "POST",
+        //         mode: 'cors',
+        //         headers: {
+        //             "Content-Type": "application/json"
+        //         },
+        //         body: JSON.stringify(responsePayload)
+        //     });
+        //     console.log(response)
+        // } catch (e) {
+        //     console.error(e);
+        // }
+
+        // const form = document.createElement('form');
+        // form.method = 'POST';
+        // form.action = 'https://umai-site-fc33abfe2d2d.herokuapp.com/home';
+
+        // const input = document.createElement('input');
+        // input.type = 'hidden';
+        // input.name = 'name';
+        // input.value = responsePayload['name'];
+        // form.appendChild(input);
+
+        // document.body.appendChild(form);
+        // form.submit();
+    }
 
     return (
         <header className="topnavBackground" id="navbar">
@@ -15,7 +70,7 @@ export default function NavBar() {
                         data-client_id="1063240373526-setg3qv32eos774o69n7erif729utnos.apps.googleusercontent.com"
                         data-context="signin"
                         data-ux_mode="popup"
-                        data-login_uri="https://umai-recipe-site-git-porting-home-calebyxus-projects.vercel.app/"
+                        data-callback="handleCredentialResponse"
                         data-auto_prompt="false">
                     </div>
                     <div className="g_id_signin"
