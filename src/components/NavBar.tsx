d14aff30ae29784fb07c0eff762154d38711d74f
calebyxu <calebxu1234@gmail.com>
2025-07-29 18:31:22 -0700

    sdfsdf

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -12,10 +12,17 @@ export default function NavBar() {
                         data-client_id="1063240373526-setg3qv32eos774o69n7erif729utnos.apps.googleusercontent.com"
                         data-context="signin"
                         data-ux_mode="popup"
-                        data-callback="handleCredentialResponse"
+                        data-login_uri="https://umai-recipe-site-git-porting-home-calebyxus-projects.vercel.app/"
                         data-auto_prompt="false">
                     </div>
-                    <div className="g_id_signin"></div>
+                    <div className="g_id_signin"
+                        data-type="standard"
+                        data-shape="rectangular"
+                        data-theme="filled_black"
+                        data-text="signin_with"
+                        data-size="large"
+                        data-logo_alignment="left">
+                    </div>
                 </div>
                 <div className='topnavLinks'>
                     <Link href='/wip'>Add Recipe</Link>
