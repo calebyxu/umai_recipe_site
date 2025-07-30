2afab3c20433bb364cc42a72f0355de274d80cb3
calebyxu <calebxu1234@gmail.com>
2025-07-29 18:29:06 -0700

    gihjkhkjhkj

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -15,15 +15,7 @@ export default function NavBar() {
                         data-callback="handleCredentialResponse"
                         data-auto_prompt="false">
                     </div>
-
-                    <div className="g_id_signin"
-                        data-type="standard"
-                        data-shape="rectangular"
-                        data-theme="outline"
-                        data-text="signin_with"
-                        data-size="large"
-                        data-logo_alignment="left">
-                    </div>
+                    <div className="g_id_signin"></div>
                 </div>
                 <div className='topnavLinks'>
                     <Link href='/wip'>Add Recipe</Link>
