4235bd3bd93996d1dc45873456c68aa5f1179d2a
calebyxu <calebxu1234@gmail.com>
2025-07-31 13:40:00 -0700

    saving changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,11 @@
 'use client'
 
+/* Next js */
 import Link from 'next/link';
 import Image from 'next/image'
-import { useState, useEffect } from 'react';
+/* React */
+import { useState, useEffect, useContext } from 'react';
+/* General */
 import '../components/component.css';
 import { GoogleLogin } from '@react-oauth/google';
 
@@ -14,6 +17,7 @@ export default function NavBar() {
     }
 
     const [buttonSize, setButtonSize] = useState<'small' | 'medium' | 'large'>('large');
+    const [responsePayload, setResponsePayload] = useState();
 
     useEffect(() => {
         const handleResize = () => {
@@ -46,20 +50,21 @@ export default function NavBar() {
     }
     async function handleCredentialResponse(response: credentialResponse) {
         if (!response.credential) {
+            console.log('there is an err');
             return;
         }
 
         console.log("Encoded JWT ID token: " + response.credential);
 
-        const responsePayload = decodeJWT(response.credential);
+        setResponsePayload(decodeJWT(response.credential));
 
-        console.log("Decoded JWT ID token fields:");
-        console.log("  Full Name: " + responsePayload.name);
-        console.log("  Given Name: " + responsePayload.given_name);
-        console.log("  Family Name: " + responsePayload.family_name);
-        console.log("  Unique ID: " + responsePayload.sub);
-        console.log("  Profile image URL: " + responsePayload.picture);
-        console.log("  Email: " + responsePayload.email);
+        // console.log("Decoded JWT ID token fields:");
+        // console.log("  Full Name: " + responsePayload.name);
+        // console.log("  Given Name: " + responsePayload.given_name);
+        // console.log("  Family Name: " + responsePayload.family_name);
+        // console.log("  Unique ID: " + responsePayload.sub);
+        // console.log("  Profile image URL: " + responsePayload.picture);
+        // console.log("  Email: " + responsePayload.email);
 
         /*fetch api doesn't work with current app since it doesn't reload page*/
         // try {
@@ -75,19 +80,6 @@ export default function NavBar() {
         // } catch (e) {
         //     console.error(e);
         // }
-
-        // const form = document.createElement('form');
-        // form.method = 'POST';
-        // form.action = 'https://umai-site-fc33abfe2d2d.herokuapp.com/home';
-
-        // const input = document.createElement('input');
-        // input.type = 'hidden';
-        // input.name = 'name';
-        // input.value = responsePayload['name'];
-        // form.appendChild(input);
-
-        // document.body.appendChild(form);
-        // form.submit();
     }
 
     return (
@@ -95,21 +87,6 @@ export default function NavBar() {
             <div className='topnav'>
                 <div id='googleLogin'>
                     <Link href="/"><div className='topnavImg'><Image src='./img/umaiBlackLogo.svg' fill={true} alt='image'></Image></div></Link>
-                    {/* <div id="g_id_onload"
-                        data-client_id="1063240373526-setg3qv32eos774o69n7erif729utnos.apps.googleusercontent.com"
-                        data-context="signin"
-                        data-ux_mode="popup"
-                        data-callback="handleCredentialResponse"
-                        data-auto_prompt="false">
-                    </div>
-                    <div className="g_id_signin"
-                        data-type="standard"
-                        data-shape="rectangular"
-                        data-theme="filled_black"
-                        data-text="signin_with"
-                        data-size="large"
-                        data-logo_alignment="left">
-                    </div> */}
                     <GoogleLogin size={buttonSize}
                         onSuccess={handleCredentialResponse}
                         onError={() => {
