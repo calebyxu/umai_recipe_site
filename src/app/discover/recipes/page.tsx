8e9429093f1b2bf67a18d5ea946ec9e3cc762d0d
calebyxu <calebxu1234@gmail.com>
2025-08-05 15:06:44 -0700

    adding recipes

--- src/app/discover/recipes/page.tsx
+++ src/app/discover/recipes/page.tsx
@@ -25,24 +25,27 @@ export default function recipe() {
         )
     } else {
         const imgRoute = '/img/recipes/' + recipes.data[id].img;
-        const recipe = recipes.data[id]; 
+        const recipe = recipes.data[id];
+
+        const instructions = recipe.instructions.split('. ');
+        const ingredients = recipe.ingredients.split('. ');
 
         return (
             <div id='container'>
                 <div id='containerLeft'>
                     <div id='containerRightImg'><Image src={imgRoute} alt='image' fill={true} objectFit='cover'></Image></div>
                     <h2>Ingredients</h2>
-                    <div id='ingredientsWrapper'>
-                        
-                    </div>
+                    <ul id='ingredientsWrapper'>
+                        {ingredients.map((row, index) => <li key={index}>{row}</li>)}
+                    </ul>
                 </div>
                 <div id='containerRight'>
                     <h1>{recipe.title}</h1>
                     <iframe src={recipe.video} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
                     <h2>Instructions</h2>
-                    <div id='ingredientsWrapper'>
-
-                    </div>
+                    <ul id='ingredientsWrapper'>
+                        {instructions.map(row => <li>{row}</li>)}
+                    </ul>
                 </div>
             </div>
         )
